ab27433fdb99d51f035d3ce2b2106db9a02ccca3
csomervil <100229796+csomervil@users.noreply.github.com>
2022-03-07 16:08:39 -0500

    now retains information from each prompt

--- Password Generator/Develop/script.js
+++ Password Generator/Develop/script.js
@@ -3,6 +3,9 @@
 
 // Get references to the #generate element
 
+
+var generateBtn = document.querySelector("#generate");
+
 var charlower = "abcdefghijklmnopqrstuvwxyz"
 
 var numbers = "0123456789"
@@ -11,35 +14,50 @@ var special = "`<>/?}{[]\.~!@#$%^&*()_-+=|,.:;'"
 
 var charupper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+function generatePassword() {
 
+  var passlenmin = window.prompt("Desired MINIMUM length of password")
 
+  var passlenmax = window.prompt("Desired MAXIMUM length of password")
 
+  var passlower= confirm("Should there be LOWERCASE?")
 
-var generateBtn = document.querySelector("#generate");
+  var passupper = confirm("Should there be UPPERCASE?")
 
-var passlenmin = window.prompt("Desired MINIMUM length of password")
+  var passnumber = confirm("Should there be NUMBERS?")
 
-var passlenmax = window.prompt("Desired MAXIMUM length of password")
+  var passspecial = confirm("Should there be SPECIAL CHARACTERS?")
 
-var passlower= confirm("Should there be LOWERCASE?")
+    if (passlower + passupper + passnumber + passspecial === 0) {
+      window.alert("You have selected no character types. Please refresh the page.")
 
-var passupper = confirm("Should there be UPPERCASE?")
+    } if (passlower === true) {
+    var pool = pool + charlower
 
-var passnumber = confirm("Should there be NUMBERS?")
+    } if (passupper === true) {
+      var pool = pool + charupper
 
-var passspecial = confirm("Should there be SPECIAL CHARACTERS?")
+    } if (passnumber === true) {
+      var pool = pool + numbers
 
-if (passlower + passupper + passnumber + passspecial === 0) {
-  window.alert("You have selected no character types. Please refresh the page.")
+    } if (passspecial === true) {
+      var pool = pool + special
+
+    }
+
+  for ( var i = 0; i < length; i++ ) {
+    password += pool.charAt(Math.floor(Math.random() * pool.length));
+  
+  }
+  return password
 }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+    passwordText.value = password;
 }
 
 // Add event listener to generate button
